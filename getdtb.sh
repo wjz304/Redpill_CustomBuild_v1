76398b05c8bd9ef1c9d3b80d2d823faa5e1a323f
Ing <wjz304@qq.com>
2022-11-29 19:18:15 +0800

    Update getdtb.sh

--- getdtb.sh
+++ getdtb.sh
@@ -2,20 +2,20 @@
 
 cd ~
 rm -rf dtbpatch dtc output.dtb output.dts
-# wget https://raw.githubusercontent.com/pocopico/rp-ext/main/dtbpatch/releases/dtbpatch
-wget https://fastly.jsdelivr.net/gh/pocopico/rp-ext@main/dtbpatch/releases/dtbpatch
+# wget --no-check-certificate https://raw.githubusercontent.com/pocopico/rp-ext/main/dtbpatch/releases/dtbpatch
+wget --no-check-certificate https://fastly.jsdelivr.net/gh/pocopico/rp-ext@main/dtbpatch/releases/dtbpatch
 chmod a+x dtbpatch
 ./dtbpatch /etc.defaults/model.dtb output.dtb
 
-# wget https://raw.githubusercontent.com/pocopico/rp-ext/main/dtbpatch/releases/dtc
-wget https://fastly.jsdelivr.net/gh/pocopico/rp-ext@main/dtbpatch/releases/dtc
+# wget --no-check-certificate https://raw.githubusercontent.com/pocopico/rp-ext/main/dtbpatch/releases/dtc
+wget --no-check-certificate https://fastly.jsdelivr.net/gh/pocopico/rp-ext@main/dtbpatch/releases/dtc
 chmod a+x dtc
 ./dtc -I dtb -O dts output.dtb > output.dts
 
 echo 请在 PC 上输入以下链接下载 dtb 文件：
-curl -w '\n' --upload-file output.dtb https://transfer.sh
+curl -fsSL --insecure -w '\n' --upload-file output.dtb https://transfer.sh
 echo 
 
 echo 请在 PC 上输入以下链接下载 dts 文件：
-curl -w '\n' --upload-file output.dts https://transfer.sh
+curl -fsSL --insecure -w '\n' --upload-file output.dts https://transfer.sh
 echo 
