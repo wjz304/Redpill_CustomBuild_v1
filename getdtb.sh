8c3dcf847c9fae183149c3bcd60341ec47e46cff
Ing <wjz304@qq.com>
2022-11-29 16:49:03 +0800

    Add files via upload

---
+++ getdtb.sh
@@ -0,0 +1,13 @@
+wget https://raw.githubusercontent.com/pocopico/rp-ext/main/dtbpatch/releases/dtbpatch
+chmod a+x dtbpatch
+./dtbpatch /etc.defaults/model.dtb output.dtb
+
+wget https://raw.githubusercontent.com/pocopico/rp-ext/main/dtbpatch/releases/dtc
+chmod a+x dtc
+dtc -I dtb -O dts output.dtb > output.dts
+
+echo 请在 PC 上输入以下链接下载 dtb 文件：
+curl --upload-file output.dtb https://transfer.sh
+
+echo 请在 PC 上输入以下链接下载 dts 文件：
+curl --upload-file output.dts https://transfer.sh
